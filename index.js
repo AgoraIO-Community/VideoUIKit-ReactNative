6f3bb203287b93e97370e017078f07fc227e2489
technophilic <svineth.face@gmail.com>
2020-04-25 18:40:02 +0530

    patch

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
 
-// import AgoraUIKit from './src/AgoraUIKit';
-// export default AgoraUIKit;
+import AgoraUIKit from './src/AgoraUIKit';
+export default AgoraUIKit;
