362c793445243fe4288868ac7c9b3f5a114f6313
HariharanIT <ghariharanit@gmail.com>
2022-12-05 15:44:43 +0530

    Updated maxvideo view

--- src/Views/MaxVideoView.tsx
+++ src/Views/MaxVideoView.tsx
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {RtcLocalView, RtcRemoteView, VideoRenderMode} from 'react-native-agora';
 import styles from '../Style';
 import PropsContext, {RenderInterface} from '../Contexts/PropsContext';
-import {View} from 'react-native';
+import {View, ViewStyle} from 'react-native';
 import useLocalUid from '../Utils/useLocalUid';
 
 const LocalView = RtcLocalView.SurfaceView;
@@ -11,30 +11,59 @@ const RemoteView = RtcRemoteView.SurfaceView;
 interface MaxViewInterface {
   user: RenderInterface;
   fallback?: React.ComponentType;
+  containerStyle?: ViewStyle;
 }
 
 const MaxVideoView: React.FC<MaxViewInterface> = (props) => {
   const {styleProps, rtcProps} = useContext(PropsContext);
   const {maxViewStyles} = styleProps || {};
   const Fallback = props.fallback;
+  const {containerStyle = {}} = props;
   const localUid = useLocalUid();
   const uid = props.user.uid === rtcProps?.screenShareUid ? 1 : props.user.uid;
   return uid === localUid ? (
     props.user.video ? (
       <LocalView
-        style={{...styles.fullView, ...(maxViewStyles as object)}}
+        style={
+          containerStyle
+            ? {
+                ...styles.fullView,
+                ...(maxViewStyles as object),
+                ...containerStyle,
+              }
+            : {
+                ...styles.fullView,
+                ...(maxViewStyles as object),
+              }
+        }
         renderMode={VideoRenderMode.Fit}
       />
     ) : Fallback ? (
       <Fallback />
     ) : (
-      <View style={{flex: 1, backgroundColor: '#000'}} />
+      <View style={[{flex: 1, backgroundColor: '#000'}, containerStyle]} />
     )
   ) : (
     <>
-      <div style={{flex: 1, display: props.user.video ? 'flex' : 'none'}}>
+      <div
+        style={{
+          flex: 1,
+          overflow: 'hidden',
+          display: props.user.video ? 'flex' : 'none',
+        }}>
         <RemoteView
-          style={{...styles.fullView, ...(maxViewStyles as object)}}
+          style={
+            containerStyle
+              ? {
+                  ...styles.fullView,
+                  ...(maxViewStyles as object),
+                  ...containerStyle,
+                }
+              : {
+                  ...styles.fullView,
+                  ...(maxViewStyles as object),
+                }
+          }
           uid={uid as number}
           renderMode={VideoRenderMode.Fit}
         />
@@ -46,7 +75,9 @@ const MaxVideoView: React.FC<MaxViewInterface> = (props) => {
           {Fallback ? (
             <Fallback />
           ) : (
-            <View style={{flex: 1, backgroundColor: '#000'}} />
+            <View
+              style={[{flex: 1, backgroundColor: '#000'}, containerStyle]}
+            />
           )}
         </>
       )}
