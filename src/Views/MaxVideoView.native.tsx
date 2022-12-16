5fbe5a039fbca82c27a5be143d4b68f040860ea8
Bhupendra Negi <bhupendra1011@gmail.com>
2022-12-16 17:09:14 +0530

    updating fill mode

--- src/Views/MaxVideoView.native.tsx
+++ src/Views/MaxVideoView.native.tsx
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {RtcLocalView, RtcRemoteView, VideoRenderMode} from 'react-native-agora';
 import styles from '../Style';
 import PropsContext, {RenderInterface} from '../Contexts/PropsContext';
-import {View} from 'react-native';
+import {View, ViewStyle} from 'react-native';
 import useLocalUid from '../Utils/useLocalUid';
 
 const LocalView = RtcLocalView.SurfaceView;
@@ -11,35 +11,45 @@ const RemoteView = RtcRemoteView.SurfaceView;
 interface MaxViewInterface {
   user: RenderInterface;
   fallback?: React.ComponentType;
+  containerStyle?: ViewStyle;
 }
 
 const MaxVideoView: React.FC<MaxViewInterface> = (props) => {
   const {styleProps, rtcProps} = useContext(PropsContext);
   const {maxViewStyles} = styleProps || {};
+  const {containerStyle = {}} = props;
   const Fallback = props.fallback;
   const localUid = useLocalUid();
   const uid = props.user.uid === rtcProps?.screenShareUid ? 1 : props.user.uid;
   return uid === localUid ? (
     props.user.video ? (
       <LocalView
-        style={{...styles.fullView, ...(maxViewStyles as object)}}
-        renderMode={VideoRenderMode.Fit}
+        style={{
+          ...styles.fullView,
+          ...(maxViewStyles as object),
+          ...containerStyle,
+        }}
+        renderMode={VideoRenderMode.FILL}
       />
     ) : Fallback ? (
       <Fallback />
     ) : (
-      <View style={{flex: 1, backgroundColor: '#000'}} />
+      <View style={[{flex: 1, backgroundColor: '#000'}, containerStyle]} />
     )
   ) : props.user.video ? (
     <RemoteView
-      style={{...styles.fullView, ...(maxViewStyles as object)}}
+      style={{
+        ...styles.fullView,
+        ...(maxViewStyles as object),
+        ...containerStyle,
+      }}
       uid={uid as number}
-      renderMode={VideoRenderMode.Fit}
+      renderMode={VideoRenderMode.FILL}
     />
   ) : Fallback ? (
     <Fallback />
   ) : (
-    <View style={{flex: 1, backgroundColor: '#000'}} />
+    <View style={[{flex: 1, backgroundColor: '#000'}, containerStyle]} />
   );
 };
 
