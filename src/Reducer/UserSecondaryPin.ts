c84e60b836ed3445f32223e35b3c92b238f67817
HariharanIT <ghariharanit@gmail.com>
2023-10-12 20:42:52 +0530

    Added reducer for secondary pinning

---
+++ src/Reducer/UserSecondaryPin.ts
@@ -0,0 +1,25 @@
+import {ActionType, ContentStateInterface} from '../Contexts/RtcContext';
+
+export default function UserSecondaryPin(
+  state: ContentStateInterface,
+  action: ActionType<'UserSecondaryPin'>,
+) {
+  let localActiveUids = [...state.activeUids];
+
+  const secondaryPinnedUid =
+    action?.value && action.value?.length ? action.value[0] : 0;
+  if (secondaryPinnedUid) {
+    const filteredData: any = localActiveUids?.filter(
+      (i) => i !== secondaryPinnedUid,
+    );
+    const [maxUid, ...minUids] = filteredData;
+    localActiveUids = [maxUid, secondaryPinnedUid, ...minUids];
+  }
+
+  return {
+    ...state,
+    secondaryPinnedUid:
+      action?.value && action.value?.length ? action.value[0] : '',
+    activeUids: localActiveUids,
+  };
+}
