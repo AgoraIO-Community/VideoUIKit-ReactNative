f4d6d48787aede1720befcd984921fa2ea69e72c
elenitaex5 <elena.martincastillo@abi.ai>
2024-01-10 14:37:11 +0100

    feat: changng timer styles and including full screen actions

--- src/Controls/Local/FullScreen.tsx
+++ src/Controls/Local/FullScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropsContext from '../../Contexts/PropsContext';
 import RtcContext from '../../Contexts/RtcContext';
 import styles from '../../Style';
@@ -13,18 +13,34 @@ const FullScreen: React.FC = () => {
   const {localBtnStyles} = styleProps || {};
   const {fullScreen} = localBtnStyles || {};
   const {dispatch} = useContext(RtcContext);
+  const [action, setAction] = useState('fullScreen')
+
+  const onPress = () => {
+    if (action === 'fullScreen') {
+      console.log('pressing fullscreen')
+      dispatch({
+        type: 'FullScreen',
+        value: [],
+      })
+      setAction('normal')
+    }
+    if (action === 'normal') {
+      console.log('pressing normal')
+      dispatch({
+        type: 'NormalScreen',
+        value: [],
+      })
+      setAction('fullScreen')
+    }
+  }
+
 
   return (
     <BtnTemplate
       name={'fullScreen'} //or normalScreen
       btnText={'Full Screen'}
       style={{...styles.localBtn, ...(fullScreen as object)}}
-      onPress={() =>
-        dispatch({
-          type: 'FullScreen',
-          value: [],
-        })
-      }
+      onPress={onPress}
     />
   );
 };
