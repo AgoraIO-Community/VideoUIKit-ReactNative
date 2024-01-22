6e8f453a7393345ba8f525a9c6806612b4436f0f
elenitaex5 <elena.martincastillo@abi.ai>
2024-01-22 10:04:49 +0100

    fix: fixing timer with dates

--- src/Controls/Local/Timer.tsx
+++ src/Controls/Local/Timer.tsx
@@ -3,19 +3,15 @@ import { Text } from 'react-native';
 
 export const Timer = () => {
   const [counter, setCounter] = useState(0);
-  const [start, setStart] = useState(0);
+  const [start, setStart] = useState(Date.now());
 
   useEffect(() => {
     const timer = setInterval(() => setCounter(Date.now() - start), 1000)
     return () => clearInterval(timer)
   }, [counter]);
 
-  useEffect(() => {
-    setStart(Date.now());
-  }, []);
-
-  const minutes = Math.trunc(counter / 60);
-  const seconds = counter - minutes * 60;
+  const minutes = Math.floor(counter / 60000);
+  const seconds = Math.floor(counter / 1000);
 
   return <Text style={{color: 'white', textAlign: 'left', alignSelf: 'center', alignItems: 'center', height: '100%'}}>{`${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`}</Text>
 }
