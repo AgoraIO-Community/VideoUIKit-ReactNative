c9c1a87b03572512a6c06d95077cf379f75d7b1c
HariharanIT <ghariharanit@gmail.com>
2024-03-18 16:20:51 +0530

    Updated end call props

--- src/Contexts/PropsContext.tsx
+++ src/Contexts/PropsContext.tsx
@@ -142,7 +142,7 @@ export interface RtcPropsInterface {
 }
 
 export interface CallbacksInterface {
-  EndCall(): void; //?
+  EndCall(isHost: boolean, isTriggeredByHost: boolean): void; //?
   FullScreen(): void; //?
   SwitchCamera(): void; //Not in reducer
   UpdateDualStreamMode(mode: DualStreamMode): void;
